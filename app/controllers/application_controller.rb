61f368f49d155282ba5f57e6f662fcaa7258de16
Rose Maina <rosewairimu27@gmail.com>
2022-10-03 22:48:00 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::API
+  #To access the cookies hash in our controllers, the ActionController::Cookies is included
   include ActionController::Cookies
 end
